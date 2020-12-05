c85684dbd8d70086e3b4eb1e191f7244c5b0f045
C-Jameson <booksncode@gmail.com>
2020-12-05 13:35:26 -0500

    registering views and urls and such

--- poetr/library/models.py
+++ poetr/library/models.py
@@ -14,13 +14,8 @@ class Poem(models.Model):
     poem_title = models.CharField(max_length=48, help_text="enter poem title")
     poem_text = models.CharField(max_length=280, help_text="enter poem text")
     poetry_genres = models.ManyToManyField('Genre', blank=True)
-    left_link = models.OneToOneField('Link')
+    links = models.ManyToManyField('self', blank=True)
 
     def __str__(self):
         return self.poem_title
-
-
-class Link(models.Model):
-    left_link = models.ForeignKey('poem', on_delete=models.CASCADE)
-    right_link = models.ForeignKey('poem', on_delete=models.CASCADE)
 
--- poetr/library/urls.py
+++ poetr/library/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import Page
 
 urlpatterns = [
-
+    path('<int:pk>/', Page.as_view(), name="Page")
 ]
