65acffb30b1b986b8720f56c3f79819ec94d768b
Jett Pavlica <jettpav@email.unc.edu>
2020-12-05 12:41:13 -0500

    added comments and polished report model

--- poetr/library/models.py
+++ poetr/library/models.py
@@ -7,9 +7,9 @@ class Genre(models.Model):
     def __str__(self):
         return self.genre_title
 
-
+# a model representing a poem within the application
 class Poem(models.Model):
-    poem_timestamp = models.DateTimeField(auto_now_add=True);
+    poem_timestamp = models.DateTimeField(auto_now_add=True)
     poem_title = models.CharField(max_length=48, help_text="enter poem title")
     poem_text = models.CharField(max_length=280, help_text="enter poem text")
     poetry_genres = models.ManyToManyField('Genre', blank=True)
@@ -24,10 +24,24 @@ class Link(models.Model):
     right_link = models.ForeignKey('poem', on_delete=models.CASCADE)
 
 
-
+# a model representing a report from the user
 class Report(models.Model):
     report_timestamp = models.DateTimeField(auto_now_add=True)
     report_text = models.CharField(max_length=400, help_text="describe why you are reporting this poem")
+
+    REPORT_TYPE_CHOICES = [
+        # use 4 letter keys
+        ('nsfw', 'NSFW'),
+        ('hrmt', 'HARASSMENT'),
+        ('cprt', 'COPYRIGHT'),
+        # and more
+    ]
+    report_type = models.CharField(
+        max_length=4,
+        choices=REPORT_TYPE_CHOICES,
+        default='cprt',
+    )
+
     report_poem = models.OneToOneField(
         Poem,
         on_delete=models.CASCADE,
