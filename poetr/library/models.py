8f4d6e43fe65c6ebc93c830aae5a2cd76bec7efc
Jett Pavlica <jettpav@email.unc.edu>
2020-12-05 12:32:11 -0500

    report model added

--- poetr/library/models.py
+++ poetr/library/models.py
@@ -2,11 +2,19 @@ from django.db import models
 
 
 class Poem(models.Model):
-
-    poem_timestamp = models.DateTimeField(auto_now_add=True);
+    poem_timestamp = models.DateTimeField(auto_now_add=True)
     poem_title = models.CharField(max_length=48, help_text="enter poem title")
     poem_text = models.CharField(max_length=280, help_text="enter poem text")
-    left_link = models.
 
     def __str__(self):
         return self.poem_title
+
+
+class Report(models.Model):
+    report_timestamp = models.DateTimeField(auto_now_add=True)
+    report_text = models.CharField(max_length=400, help_text="describe why you are reporting this poem")
+    report_poem = models.OneToOneField(
+        Poem,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
