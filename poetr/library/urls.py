384c3319c9e1ff229bd618c3ebd9d5f6f5e6eca5
C-Jameson <booksncode@gmail.com>
2020-12-05 15:56:27 -0500

    random start page

--- poetr/library/urls.py
+++ poetr/library/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Page
+from .views import Page, Random
 
 urlpatterns = [
-    path('<int:pk>/', Page.as_view(), name="Page")
+    path('<int:pk>/', Page.as_view(), name="Page"),
+    path('', Random, name="Random")
 ]
--- poetr/library/views.py
+++ poetr/library/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Genre, Poem
+import random
+r = random.Random()
+
+
+def Random(request):
+    page = Poem.objects.order_by('?')[:1][0]
+    return redirect(page)
 
 
 class Page(DetailView):
@@ -12,9 +19,9 @@ class Page(DetailView):
         context = {'title': poem.title,
                    'author': poem.author,
                    'timestamp': poem.timestamp,
-                   'genres': poem.genres,
+                   'genres': poem.genres.all(),
                    'text': poem.text,
-                   'links': poem.links}
+                   'links': poem.links.all()}
 
         return context
 
