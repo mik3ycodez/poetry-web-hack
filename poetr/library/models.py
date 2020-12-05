fbbe0c84cb60802bfcd853d4d0410f6edc6eb0e4
C-Jameson <booksncode@gmail.com>
2020-12-05 12:04:04 -0500

    model first commit
    first crack at models.py

--- poetr/library/models.py
+++ poetr/library/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Poem(models.Model):
+
+    poem_timestamp = models.DateTimeField(auto_now_add=True);
+    poem_title = models.CharField(max_length=48, help_text="enter poem title")
+    poem_text = models.CharField(max_length=280, help_text="enter poem text")
+    left_link = models.
+
+    def __str__(self):
+        return self.poem_title
