3849ea65ea50c81962650956b2b8ac247df81d43
Jett Pavlica <jettpav@email.unc.edu>
2020-12-05 12:41:13 -0500

    added comments and polished report model

--- poetr/library/models.py
+++ poetr/library/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# a model representing a poem within the application
 class Poem(models.Model):
     poem_timestamp = models.DateTimeField(auto_now_add=True)
     poem_title = models.CharField(max_length=48, help_text="enter poem title")
@@ -10,9 +11,24 @@ class Poem(models.Model):
         return self.poem_title
 
 
+# a model representing a report from the user
 class Report(models.Model):
     report_timestamp = models.DateTimeField(auto_now_add=True)
     report_text = models.CharField(max_length=400, help_text="describe why you are reporting this poem")
+
+    REPORT_TYPE_CHOICES = [
+        # use 4 letter keys
+        ('nsfw', 'NSFW'),
+        ('hrmt', 'HARASSMENT'),
+        ('cprt', 'COPYRIGHT'),
+        # and more
+    ]
+    report_type = models.CharField(
+        max_length=4,
+        choices=REPORT_TYPE_CHOICES,
+        default='cprt',
+    )
+
     report_poem = models.OneToOneField(
         Poem,
         on_delete=models.CASCADE,
