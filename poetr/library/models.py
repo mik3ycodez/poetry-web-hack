cfd03bba382c6f19ac20c2f98918d082d27e0125
C-Jameson <booksncode@gmail.com>
2020-12-05 12:38:54 -0500

    models

--- poetr/library/models.py
+++ poetr/library/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    genre_title = models.CharField(max_length=18)
+
+    def __str__(self):
+        return self.genre_title
+
+
 class Poem(models.Model):
 
-    poem_timestamp = models.DateTimeField(auto_now_add=True);
+    poem_timestamp = models.DateTimeField(auto_now_add=True)
     poem_title = models.CharField(max_length=48, help_text="enter poem title")
     poem_text = models.CharField(max_length=280, help_text="enter poem text")
-    left_link = models.
+    poetry_genres = models.ManyToManyField('Genre', blank=True)
+    left_link = models.OneToOneField('Link')
 
     def __str__(self):
         return self.poem_title
+
+
+class Link(models.Model):
+    left_link = models.ForeignKey('poem', on_delete=models.CASCADE)
+    right_link = models.ForeignKey('poem', on_delete=models.CASCADE)
+
