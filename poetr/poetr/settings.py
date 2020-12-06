3292d721eeb5f6b0cfa4d9e2ebe07571ea6264e7
C-Jameson <booksncode@gmail.com>
2020-12-06 01:25:14 -0500

    no password

--- poetr/poetr/settings.py
+++ poetr/poetr/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'poetr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '/cloudsql/poetry-hack-django:us-central1:poetr-sql',
+        'USER': 'poetr',
+        'PASSWORD': '[YOUR-PASSWORD]',
+        'NAME': 'poetry',
     }
 }
 
