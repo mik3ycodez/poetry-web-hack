0c865b911dbfaa7ccd939407faf8ed888749bafc
C-Jameson <booksncode@gmail.com>
2020-12-06 04:33:54 -0500

    found missing textbox

--- poetr/library/forms.py
+++ poetr/library/forms.py
@@ -5,7 +5,7 @@ class NewPoemForm(forms.Form):
     title = forms.CharField(max_length=48, label='A title for your thoughts?', required=False)
     text = forms.CharField(widget=forms.Textarea, max_length=280, label='Go ahead and scream:', required=False)
     author = forms.CharField(max_length=48, label="Who are you (not) really?", required=False)
-    genres = forms.CharField(max_length=280, label='Think inside the comma separated boxes:',
+    genres = forms.CharField(max_length=280, label='Think inside the comma separated box:',
                              required=False)
 
     def clean_genres(self):
