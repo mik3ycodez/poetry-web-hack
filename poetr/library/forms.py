9ed0cc59228ccdf30fc6596e0672fd51cadddbbb
C-Jameson <booksncode@gmail.com>
2020-12-06 02:53:32 -0500

    changing prompt

--- poetr/library/forms.py
+++ poetr/library/forms.py
@@ -3,9 +3,9 @@ from django import forms
 
 class NewPoemForm(forms.Form):
     title = forms.CharField(max_length=48, label='A title for your thoughts?', required=False)
-    text = forms.CharField(max_length=280, label='go ahead and scream', required=False)
+    text = forms.CharField(max_length=280, label='Go ahead and scream:', required=False)
     author = forms.CharField(max_length=48, label="Who are you (not) really?", required=False)
-    genres = forms.CharField(max_length=280, label='put your work into lots of little comma separated boxes',
+    genres = forms.CharField(max_length=280, label='Think inside the comma separated boxes:',
                              required=False)
 
     def clean_genres(self):
