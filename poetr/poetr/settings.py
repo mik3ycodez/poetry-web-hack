ec689f1f8387a2aced92072a7a2d2840eadb9a4b
C-Jameson <booksncode@gmail.com>
2020-12-06 01:31:14 -0500

    second better no pass

--- poetr/poetr/settings.py
+++ poetr/poetr/settings.py
@@ -74,15 +74,35 @@ WSGI_APPLICATION = 'poetr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '/cloudsql/poetry-hack-django:us-central1:poetr-sql',
-        'USER': 'poetr',
-        'PASSWORD': '[YOUR-PASSWORD]',
-        'NAME': 'poetry',
+# [START db_setup]
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/poetry-hack-django:us-central1:poetr-sql',
+            'USER': 'poetr',
+            'PASSWORD': '[YOUR-PASSWORD]',
+            'NAME': 'poetry',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect
+    # to Cloud SQL via the proxy.  To start the proxy via command line:
+    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'poetry',
+            'USER': 'poetr',
+            'PASSWORD': '[YOUR-PASSWORD]',
+        }
+    }
+# [END db_setup]
 
 
 # Password validation
